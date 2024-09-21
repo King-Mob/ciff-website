1e3ffc386c8c3d4efef95183cd4811cbe737da2c
John Evans <38507954+King-Mob@users.noreply.github.com>
2024-09-21 14:20:38 +0100

    create site structure, title animation

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const title = document.getElementById("title");
+
+const titleText = title.innerHTML;
+
+const titleList = titleText.split("");
+
+console.log(titleList)
+
+title.innerHTML = "";
+
+titleList.forEach((char, index) => {
+    const span = document.createElement("span");
+    span.innerHTML = char;
+    span.classList.add("title-character");
+    span.style.animationDelay = `-${index / 2}s`;
+    span.style.animationDuration = "20s";
+    title.appendChild(span);
+})
