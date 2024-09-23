c1f975935bf0ca31ddc7b0fb63310acb3f57ab37
John Evans <38507954+King-Mob@users.noreply.github.com>
2024-09-23 17:04:01 +0100

    add mobile styles, icons, buttons

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
+//create title
+
 const title = document.getElementById("title");
 
 const titleText = title.innerHTML;
 
 const titleList = titleText.split("");
-
-console.log(titleList)
 
 title.innerHTML = "";
 
@@ -16,3 +16,24 @@ titleList.forEach((char, index) => {
     span.style.animationDuration = "20s";
     title.appendChild(span);
 })
+
+// create more info buttons
+
+const toggleInfoButton = (button) => {
+    const info = button.parentElement.children[0];
+
+    if (button.innerHTML === "more info") {
+        info.style.display = "inline";
+        button.innerHTML = "less info";
+    }
+    else {
+        info.style.display = "none";
+        button.innerHTML = "more info";
+    }
+}
+
+const moreInfoButtons = document.getElementsByClassName("info-button");
+
+for (const button of moreInfoButtons) {
+    button.onclick = () => toggleInfoButton(button);
+}
