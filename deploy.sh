859ec4601593536819adcb928d4fec94afacdcac
John Evans <38507954+King-Mob@users.noreply.github.com>
2024-09-23 18:17:27 +0100

    update deploy instructions

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 git pull
-cp -t /var/www/ciff.wobbly.app/public_html BlurWeb-Medium_W03_Regular.ttf ciff24transparent.png index.html instagram-icon.svg scripts.js styles.css 
+cp -t /var/www/ciff.wobbly.app/public_html BlurWeb-Medium_W03_Regular.ttf canvas_animation.js ciff24transparent.png index.html instagram-icon.svg scripts.js styles.css 
