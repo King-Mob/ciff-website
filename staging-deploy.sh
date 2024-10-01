1fa46949859b339871964734fcc8deaab82cc976
John Evans <38507954+King-Mob@users.noreply.github.com>
2024-10-01 16:21:23 +0100

    add koop logo

--- staging-deploy.sh
+++ staging-deploy.sh
@@ -1,2 +1,2 @@
 git pull
-cp -t /var/www/ciff.wobbly.app/public_html BlurWeb-Medium_W03_Regular.ttf calverts.png canvas_animation.js ciff24favicon.jpg ciff24transparent.png index.html instagram-icon.svg omnibus.png picturehouse.jpg scripts.js sib-styles.css styles.css verve.svg
+cp -t /var/www/ciff.wobbly.app/public_html BlurWeb-Medium_W03_Regular.ttf calverts.png canvas_animation.js ciff24favicon.jpg ciff24transparent.png index.html instagram-icon.svg koop.png omnibus.png picturehouse.jpg scripts.js sib-styles.css styles.css verve.svg
